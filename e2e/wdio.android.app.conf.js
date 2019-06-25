92eae256386147fa65e2b791f3beb7ac8f909506
siegklenes beulke <siegklenes.beulke@totvs.com.br>
2019-06-25 14:37:41 -0300

    cleanup, folder fixes

--- e2e/wdio.android.app.conf.js
+++ e2e/wdio.android.app.conf.js
@@ -12,7 +12,7 @@ config.specs = [
 // ============
 // For all capabilities please check
 // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
-config.capabilities = [
+config.capabilities = [ // Para mais de um emulador na mesma maquina, adicionar na lista
     {
         // The defaults you need to have in your config
         platformName: 'Android',
@@ -20,16 +20,15 @@ config.capabilities = [
         // For W3C the appium capabilities need to have an extension prefix
         // http://appium.io/docs/en/writing-running-appium/caps/
         // This is `appium:` for all Appium Capabilities which can be found here
-        //'appium:chromedriverChromeMappingFile': 'C:\\wms\\SL-WMSIonic\\chrome.json',
-        //'appium:chromedriverExecutableDir': 'C:\\wms\\SL-WMSIonic\\chromedrivers\\',
+        //'appium:chromedriverChromeMappingFile': 'C:\\wms\\SL-WMSIonic\\chrome.json', // Mapeia versão de chrome vs chromedriver, se necessário
+        //'appium:chromedriverExecutableDir': 'C:\\wms\\SL-WMSIonic\\chromedrivers\\', // Diretório com todas as versões de chrome adicionais, appium usa chrome.json e mais uma tabela própria para achar a versão correta
         'appium:appPackage': 'io.ionic.starter',
         'appium:appActivity': '.MainActivity',
-        'appium:app': '/root/tmp/app-debug.apk',
+        'appium:app': '/root/tmp/app-debug.apk',// Caso deseje que o appium instale o apk
         'appium:autoAcceptAlerts': 'true',
         'appium:autoGrantPermissions': 'true',
         'appium:autoWebview': 'true',
         'appium:autoWebviewTimeout': 360000,
-        //'appium:chromedriverChromeMappingFile': 'C:\\wms\\SL-WMSIonic\\chrome.json',
         'appium:deviceName': 'emulator-5554',
         'appium:orientation': 'PORTRAIT',
         // `automationName` will be manatory, see
@@ -38,8 +37,6 @@ config.capabilities = [
         'appium:skipServerInstallation': false,
         'appium:uiautomator2ServerLaunchTimeout': 60000,
         'appium:uiautomator2ServerInstallTimeout': 60000,
-        // The path to the app
-        // 'appium:app': join(process.cwd(), './apps/Android-NativeDemoApp-0.2.1.apk'),
         // Read the reset strategies very well, they differ per platform, see
         // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
         'appium:noReset': true,
